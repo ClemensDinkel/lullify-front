756b87d7f4d296e53610d61c6193ed694f72c898
Utsavi009 <utsavikathiriya@gmail.com>
2021-06-14 22:17:01 +0200

    registration of user functionality completed

--- src/components/CreatorRegister.jsx
+++ src/components/CreatorRegister.jsx
@@ -1,52 +1,200 @@
+import { Card, Col, Form, Button } from "react-bootstrap";
+import { register } from "./AuthFunctions";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import queryString from "query-string";
 
-import { Card, Col, Form, FormControl, Button } from 'react-bootstrap'
+const CreatorRegister = () => {
+  const [ccRegister, setCcRegister] = useState({
+    first_name: "",
+    last_name: "",
+    user_name: "",
+    email: "",
+    password: "",
+    phone: "",
+    city_code: "",
+    city_name: "",
+    country: "",
+    company: "",
+    requested_to_be_cc: true,
+    errors: {},
+  });
 
-const CreratorRegister = () => {
-    return (
-        <>
-        CreratorRegister
-        <Card style={{ width: '30rem' }}>
-                <Card.Body>
-                <Form>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>First name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter First name" name="first_name"  />
-                    </Form.Group>
+  let history = useHistory();
 
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Last name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Last name" name="last_name"  />
-                    </Form.Group>
-                    
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>User name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User name" name="user_name"  />
-                    </Form.Group>
-                </Form.Row>
+  const onChange = (e) => {
+    let keyName = e.target.name;
+    let value = e.target.value;
+    setCcRegister((previous) => {
+      return {
+        ...previous,
+        [keyName]: value,
+      };
+    });
+  };
 
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email"  />
-                    </Form.Group>
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" />
-                    </Form.Group>
-                </Form.Row>
+    const newUser = {
+      firs_tname: ccRegister.firstname,
+      last_name: ccRegister.lastname,
+      user_name: ccRegister.user_name,
+      email: ccRegister.email,
+      password: ccRegister.password,
+      phone: ccRegister.phone,
+      city_code: ccRegister.city_code,
+      city_name: ccRegister.city_name,
+      country: ccRegister.country,
+      company: ccRegister.company,
+      requested_to_be_cc: true,
+    };
 
-                <Form.Row>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                </Form.Row>
-            </Form>
-                </Card.Body>
-            </Card>
-        </>
-    )
-}
+    register(queryString.stringify(newUser)).then((res) => {
+      history.push(`/login`);
+    });
+  };
 
-export default CreratorRegister
+  return (
+    <>
+      <p>CreratorRegister</p>
+      <Card style={{ width: "30rem" }}>
+        <Card.Body>
+          <Form onSubmit={onSubmit}>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>First name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter First name"
+                  name="first_name"
+                  value={ccRegister.first_name}
+                  onChange={onChange}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Last name"
+                  name="last_name"
+                  value={ccRegister.last_name}
+                  onChange={onChange}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>User name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter User name"
+                  name="user_name"
+                  value={ccRegister.user_name}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  value={ccRegister.email}
+                  onChange={onChange}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={ccRegister.password}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Phone no."
+                  name="phone"
+                  value={ccRegister.phone}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Postal Code"
+                  name="city_code"
+                  value={ccRegister.city_code}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter City"
+                  name="city_name"
+                  value={ccRegister.city_name}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Country"
+                  name="country"
+                  value={ccRegister.country}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Company</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Company name"
+                  name="company"
+                  value={ccRegister.company}
+                  onChange={onChange}
+                />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form.Row>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default CreatorRegister;
