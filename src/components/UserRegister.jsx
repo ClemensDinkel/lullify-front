ad9f9a892432b82e2b70358ca4d52e6f485c4c09
Ballerheinz <cle.dinkel@gmail.com>
2021-06-15 09:24:09 +0200

    merge conf inc

--- src/components/UserRegister.jsx
+++ src/components/UserRegister.jsx
@@ -1,8 +1,31 @@
 import { Card, Col, Form, FormControl, Button } from 'react-bootstrap'
 import { register } from './AuthFunctions';
+import { useState } from 'react';
 
 
 const UserRegister = () => {
+
+    const [newRegister, setNewRegister] = usetState({
+        first_name: "",
+        last_name: "",
+        user_name: "",
+        email: "",
+        password: ""
+    })
+
+    const onChange = (e) => {
+        let keyName = e.target.name;
+        let value = e.target.value;
+        setNewRegister((previous) => {
+          return {
+            ...previous,
+            [keyName]: value,
+          };
+        });
+      }
+
+
+
     return (
         <>
         <p>User Registraion</p>
@@ -12,29 +35,29 @@ const UserRegister = () => {
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>First name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter First name" name="first_name"/>
+                        <Form.Control type="text" placeholder="Enter First name" name="first_name" value={newRegister.first_name} onChange={onChange} />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword">
                         <Form.Label>Last name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Last name" name="last_name"/>
+                        <Form.Control type="text" placeholder="Enter Last name" name="last_name" value={newRegister.last_name} onChange={onChange} />
                     </Form.Group>
                     
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>User name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User name" name="user_name" />
+                        <Form.Control type="text" placeholder="Enter User name" name="user_name"  value={newRegister.user_name} onChange={onChange} />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email" />
+                        <Form.Control type="email" placeholder="Enter email" name="email" value={newRegister.email} onChange={onChange} />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" />
+                        <Form.Control type="password" placeholder="Password" name="password" value={newRegister.password} onChange={onChange} />
                     </Form.Group>
                 </Form.Row>
 
