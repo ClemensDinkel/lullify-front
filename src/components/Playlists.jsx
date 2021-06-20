c03cd85949dec5b543268def6e2161012ec8ae2a
Utsavi009 <utsavikathiriya@gmail.com>
2021-06-20 13:06:40 +0200

    code format

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,16 +1,16 @@
 const Playlists = () => {
-    return (
-        <>
-            <h1>Playlists</h1>
-            <ul>
-                <li>videos1</li>
-                <li>videos2</li>
-                <li>videos3</li>
-                <li>videos4</li>
-                <li>videos5</li>
-            </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Playlists</h1>
+      <ul>
+        <li>videos1</li>
+        <li>videos2</li>
+        <li>videos3</li>
+        <li>videos4</li>
+        <li>videos5</li>
+      </ul>
+    </>
+  );
+};
 
-export default Playlists
+export default Playlists;
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,70 +1,69 @@
-import ReactPlayer from 'react-player/lazy'
-import {Container, Col, Row, Button} from 'react-bootstrap'
-import '../App.css'
-import {useState, useEffect} from 'react'
-import {HiOutlineDocumentReport} from 'react-icons/hi'
-import {HiDocumentReport} from 'react-icons/hi'
+import ReactPlayer from "react-player/lazy";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import "../App.css";
+import { useState, useEffect } from "react";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { HiDocumentReport } from "react-icons/hi";
 
+const Video = ({ video }) => {
+  const [readMore, setReadMore] = useState(false);
 
-const Video = ({video}) => {
+  const secToMinConerter = (n) => {
+    return n / 60;
+  };
 
-    const [readMore, setReadMore] = useState(false)
+  return (
+    <>
+      <div className="video-container">
+        <Container>
+          <Row>
+            <Col>
+              <ReactPlayer
+                controls
+                className="react-player"
+                url={video[0].video_url}
+                muted={false}
+                playing={true}
+                loop={true}
+                width="100%"
+                height="600px"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="video-description">
+                <h3>{video[0].title}</h3>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Button
+                    variant="light"
+                    onClick={() => setReadMore(!readMore)}
+                  >
+                    {!readMore ? <b>Show More</b> : <b>Show less</b>}
+                  </Button>
+                  <Button variant="light">
+                    <HiOutlineDocumentReport />
+                    <HiDocumentReport />
+                  </Button>
+                </div>
 
-    const secToMinConerter = (n) => {
-        return n/60;
-    }
+                {readMore ? (
+                  <div style={{ backgroundColor: "rgba(0,0,0,0.2)" }}>
+                    <h6>
+                      Duration: {secToMinConerter(video[0].duration)} mins
+                    </h6>
+                    <p>{video[0].short_description}</p>
+                  </div>
+                ) : null}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div className='video-container'>
-            <Container>
-                <Row>
-                    <Col>
-                    <ReactPlayer
-                        controls
-                        className='react-player'
-                        url={video[0].video_url}
-                        muted={false}
-                        playing={true}
-                        loop={true}
-                        width='100%'
-                        height='600px'
-                    />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <div className='video-description'>
-                        <h3>{video[0].title}</h3>
-                        <div
-                        style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Button variant="light" onClick={()=> setReadMore(!readMore)}>
-                            {!readMore ? <b>Show More</b> : <b>Show less</b>}
-                        </Button>
-                        <Button variant="light">
-                            
-                            <HiOutlineDocumentReport/> 
-                            <HiDocumentReport/>
-                        </Button>
-                        </div>
-
-
-                        {
-                            readMore ? 
-                            <div
-                            style={{ backgroundColor: 'rgba(0,0,0,0.2)' }}>
-                            <h6>Duration: {secToMinConerter(video[0].duration)} mins</h6>
-                            <p>{video[0].short_description}</p>
-                            </div> :
-                            null
-                        }
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            </div>
-        </>
-    )
-}
-
-export default Video
+export default Video;
