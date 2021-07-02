33a6effc93743a0ba57ecd09ed4680b93025bf90
Utsavi009 <utsavikathiriya@gmail.com>
2021-07-02 23:46:38 +0200

    change api root to heroku

--- src/api.js
+++ src/api.js
@@ -1,92 +1,67 @@
 import axios from "axios";
-const root = "http://localhost:3001";
-// const root = 'https://tranquil-reaches-12289.herokuapp.com';
+//const root = "http://localhost:3001";
+const root = "https://tranquil-reaches-12289.herokuapp.com";
 
 const api = {
   loginUser: async (user) => {
-    return await axios.post(`/login`,
-      user, {
+    return await axios.post(`/login`, user, {
       /* withCredentials: true, */
-      baseURL: root
+      baseURL: root,
     });
   },
 
   logoutUser: async () => {
-    return await axios.get(
-      `${root}/logout`, {
+    return await axios.get(`${root}/logout`, {
       /* withCredentials: true, */
     });
   },
 
   registerUser: async (newUser) => {
-    return await axios.post(
-      `${root}/register`,
-      newUser
-    );
+    return await axios.post(`${root}/register`, newUser);
   },
 
   refreshUserToken: async () => {
     return await axios.post(
       `${root}/refresh`,
-      {},
+      {}
       /* {withCredentials:true} */
-    )
+    );
   },
 
   fetchSingleUser: async (user_id) => {
-    return await axios.get(
-      `${root}/users/${user_id}`
-    );
+    return await axios.get(`${root}/users/${user_id}`);
   },
 
   updateUser: async (user_id, userData) => {
-    return await axios.put(
-      `${root}/users/${user_id}`,
-      userData
-    );
+    return await axios.put(`${root}/users/${user_id}`, userData);
   },
 
   demoteUser: async (user_id) => {
-    return await axios.put(
-      `${root}/users/${user_id}/demote`
-    );
+    return await axios.put(`${root}/users/${user_id}/demote`);
   },
 
   promoteUser: async (user_id) => {
-    return await axios.put(
-      `${root}/users/${user_id}/promote`
-    );
+    return await axios.put(`${root}/users/${user_id}/promote`);
   },
 
   getAllUsers: async () => {
-    return await axios.get(
-      `${root}/users`
-    );
+    return await axios.get(`${root}/users`);
   },
 
   getAllRequests: async () => {
-    return await axios.get(
-      `${root}/requests`
-    );
+    return await axios.get(`${root}/requests`);
   },
 
   deleteRequest: async (request_id) => {
-    return await axios.delete(
-      `${root}/requests/${request_id}`
-    );
+    return await axios.delete(`${root}/requests/${request_id}`);
   },
 
   createPlaylist: async (newPlaylist) => {
-    return await axios.post(
-      `${root}/playlists`,
-      newPlaylist
-    );
+    return await axios.post(`${root}/playlists`, newPlaylist);
   },
 
   getPlaylist: async (user_id) => {
-    return await axios.get(
-      `${root}/users/${user_id}/playlists`
-    );
+    return await axios.get(`${root}/users/${user_id}/playlists`);
   },
 
   addVideoToPlaylist: async (user_id, playlist_id, videoId) => {
@@ -110,17 +85,11 @@ const api = {
   },
 
   addVideoToFavorite: async (user_id, videoId) => {
-    return await axios.put(
-      `${root}/users/${user_id}/favorites`,
-      videoId
-    );
+    return await axios.put(`${root}/users/${user_id}/favorites`, videoId);
   },
 
   removeVideoFromFavorite: async (user_id, videoId) => {
-    return await axios.put(
-      `${root}/users/${user_id}/removefavorites`,
-      videoId
-    );
+    return await axios.put(`${root}/users/${user_id}/removefavorites`, videoId);
   },
 
   getVideos: async () => {
@@ -132,34 +101,23 @@ const api = {
   },
 
   reportVideo: async (video_id, reportData) => {
-    return await axios.put(`${root}/videos/${video_id}/report`,
-      reportData
-    );
+    return await axios.put(`${root}/videos/${video_id}/report`, reportData);
   },
 
   unReportVideo: async (video_id, deReportData) => {
-    return await axios.put(`${root}/videos/${video_id}/unreport`,
-      deReportData
-    );
+    return await axios.put(`${root}/videos/${video_id}/unreport`, deReportData);
   },
 
   addVideos: async (newVideo) => {
-    return await axios.post(
-      `${root}/videos`,
-      newVideo
-    );
+    return await axios.post(`${root}/videos`, newVideo);
   },
 
   getUploaderAllVideos: async (user_id) => {
-    return await axios.get(
-      `${root}/videos/byUploader/${user_id}`
-    );
+    return await axios.get(`${root}/videos/byUploader/${user_id}`);
   },
 
   deleteUploaderVideo: async (user_id, video_id) => {
-    return await axios.delete(
-      `${root}/users/${user_id}/videos/${video_id}`
-    );
+    return await axios.delete(`${root}/users/${user_id}/videos/${video_id}`);
   },
 
   updateUploaderVideo: async (user_id, video_id, videoData) => {
