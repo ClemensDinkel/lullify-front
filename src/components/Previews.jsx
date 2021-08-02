1d19ceb9714342abdf4b9fb03d56db43b57f3189
Ballerheinz <cle.dinkel@gmail.com>
2021-08-02 10:05:44 +0200

    reorder api calls in api.js and make the code consistent

--- src/components/Previews.jsx
+++ src/components/Previews.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react'
 import '../App.css'
 import { VideoContext } from '../context/VideoContext'
-import { Card } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { PlaylistContext } from '../context/PlaylistContext';
 import useBreakpoint from '../customHooks/useBreakpoint';
@@ -12,14 +12,15 @@ const Previews = () => {
   const [autoPlaylist, setAutoPlaylist] = useContext(PlaylistContext)
   const point = useBreakpoint();
   const [margin, setMargin] = useState(1)
+  const [loadingVideos, setLoadingVideos] = useState(true)
 
   useEffect(() => {
-    const margin = point === "xl" ? 20 : point === "lg" ? 10 : point === "md" ? 5 : 0  
+    const margin = point === "xl" ? 20 : point === "lg" ? 10 : point === "md" ? 5 : 0
     setMargin(margin)
-  },[point])
+  }, [point])
 
   const playPlaylist = () => {
-    const autoPlay=[]
+    const autoPlay = []
     videos.forEach(video => autoPlay.push(video._id))
     setAutoPlaylist(autoPlay)
   }
@@ -27,21 +28,25 @@ const Previews = () => {
   return (
     <div className="previews-container">
       {
-        videos && videos.map((video, index) => {
+        videos ? videos.map((video, index) => {
           return (
             <div>
-              <Card key={index} style={{background: "rgba(0,0,0,0.1)", margin: `${margin}px`}} text="white" className="previews-card-container" onClick={()=> playPlaylist()}>
+              <Card key={index} style={{ background: "rgba(0,0,0,0.1)", margin: `${margin}px` }} text="white" className="previews-card-container" onClick={() => playPlaylist()}>
                 <Link to={`/player/${video._id}`}>
                   <img variant="top" src={video.video_img_url} height="140px" width="100%" />
                 </Link>
-                <Card.Body style={{textAlign: "left"}}>
-                  <Card.Title style={{fontFamily: "cursive"}}>{video.title}</Card.Title>
+                <Card.Body style={{ textAlign: "left" }}>
+                  <Card.Title style={{ fontFamily: "cursive" }}>{video.title}</Card.Title>
                   <Card.Text >{video.artist}</Card.Text>
                 </Card.Body>
               </Card>
             </div>
           )
-        })
+        }) :
+          <div>
+            <p>Loading....</p>
+            <Spinner animation="border" variant="light" />
+          </div>
       }
     </div >
   )
