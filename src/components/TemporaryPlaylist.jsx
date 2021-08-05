1b214a7d2c2bed6b8694dc73cc915afb0f99ad7f
Ballerheinz <cle.dinkel@gmail.com>
2021-08-05 13:50:14 +0200

    introduce dnd context and droppable on temp playlists

--- src/components/TemporaryPlaylist.jsx
+++ src/components/TemporaryPlaylist.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { VideoContext } from "../context/VideoContext";
 import { PlaylistContext } from "../context/PlaylistContext";
 import moon_image from "../images/moon2.png"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const TemporaryPlaylist = () => {
   const [videos] = useContext(VideoContext);
@@ -73,52 +74,58 @@ const TemporaryPlaylist = () => {
           </Nav.Link>
         </h2>
         <div style={{ textAlign: "left" }}>
-          <ul>
-            {temporaryPlaylist.map(
-              (listVideo, listVideoIndex) => {
-                return (
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      width: "100%"
-                    }}
-                  >
-                    <li
-                      key={listVideoIndex}
-                      style={{
-                        color: "antiquewhite",
-                        display: "flex",
-                        flexWrap: "wrap",
-                        width: "80%",
-                        textAlign: "left"
-                      }}
-                      onClick={() =>
-                        playSingleVideo(listVideo._id)
-                      }
-                    >
-                      <Nav.Link
-                        as={Link}
-                        to={`/player/${listVideo._id}`}
-                      >
-                        <h6 style={{ color: "antiquewhite" }}>
-                          {listVideo.title}
-                        </h6>
-                      </Nav.Link>
-                    </li>
-                    <Button
-                      type="button"
-                      style={{ maxHeight: "40px" }}
-                      variant="dark"
-                      onClick={() => { removeVideo(listVideo._id); }}
-                    >
-                      <MdDelete />
-                    </Button>
-                  </div>
-                );
-              }
-            )}
-          </ul>
+          <DragDropContext>
+            <Droppable droppableId="temp-playlist">
+              {(provided, snapshot) => (
+                <ul ref={provided.innerRef}>
+                  {temporaryPlaylist.map(
+                    (listVideo, listVideoIndex) => {
+                      return (
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "100%"
+                          }}
+                        >
+                          <li
+                            key={listVideoIndex}
+                            style={{
+                              color: "antiquewhite",
+                              display: "flex",
+                              flexWrap: "wrap",
+                              width: "80%",
+                              textAlign: "left"
+                            }}
+                            onClick={() =>
+                              playSingleVideo(listVideo._id)
+                            }
+                          >
+                            <Nav.Link
+                              as={Link}
+                              to={`/player/${listVideo._id}`}
+                            >
+                              <h6 style={{ color: "antiquewhite" }}>
+                                {listVideo.title}
+                              </h6>
+                            </Nav.Link>
+                          </li>
+                          <Button
+                            type="button"
+                            style={{ maxHeight: "40px" }}
+                            variant="dark"
+                            onClick={() => { removeVideo(listVideo._id); }}
+                          >
+                            <MdDelete />
+                          </Button>
+                        </div>
+                      );
+                    }
+                  )}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
       </div>
 
