9ddf1db5000595b40fc9bf09dad7bf47ad66a7d6
Ballerheinz <cle.dinkel@gmail.com>
2021-09-08 21:19:12 +0200

    ..

--- src/components/About/MediaLink.jsx
+++ src/components/About/MediaLink.jsx
@@ -1,7 +1,7 @@
 import { AiOutlineGithub } from "react-icons/ai"
 import { FaLinkedin, FaXingSquare, FaYoutubeSquare } from "react-icons/fa"
 
-export const MediaLink = ({medium}) => {
+export const MediaLink = ({ medium }) => {
   console.log(medium[1])
   return (
     <h2>
@@ -11,18 +11,18 @@ export const MediaLink = ({medium}) => {
         rel="noreferrer"
         style={{ margin: "7px", color: "black" }}
       >
+        {
+          medium[0] === "linkedIn" ?
+            <FaLinkedin /> :
+            medium[0] === "gitHub" ?
+              <AiOutlineGithub /> :
+              medium[0] === "xing" ?
+                <FaXingSquare /> :
+                medium[0] === "youTube" ?
+                  <FaYoutubeSquare /> :
+                  <p>{medium[0]}</p>
+        }
       </a>
-      {
-      medium[0] === "linkedIn" ?
-      <FaLinkedin /> :
-      medium[0] === "gitHub" ?
-      <AiOutlineGithub /> :
-      medium[0] === "xing" ?
-      <FaXingSquare /> :
-      medium[0] === "youTube" ?
-      <FaYoutubeSquare/>:
-      <p>{medium[0]}</p>
-      }
     </h2>
   )
 }
